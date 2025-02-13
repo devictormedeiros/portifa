5aab55fcf048c7ec29e045292d06b9e24720dc76
Matheus <matheus@seox.com.br>
2025-02-13 00:55:43 -0300

    Implementção de pesquisa do scroll.

--- src/app/components/Home/Skills/TabsSkill.js
+++ src/app/components/Home/Skills/TabsSkill.js
@@ -1,19 +1,57 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Tab, TabPanel, Tabs, TabsBody, TabsHeader } from "@material-tailwind/react";
 import Accordion from "../../Accordion/Accordion";
 import CardSkill from "./CardSkill";
 
 const TabsSkill = ({ skills, title }) => {
     const [activeTab, setActiveTab] = useState(0);
+    const containerRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(0);
 
     useEffect(() => {
         if (skills.length > 0) setActiveTab(0); // Define o primeiro item como ativo
     }, [skills]);
 
+    const startDragging = (e) => {
+        setIsDragging(true);
+        e.preventDefault();
+        const pageX = e.touches ? e.touches[0].pageX : e.pageX;
+        setStartX(pageX - (containerRef.current?.scrollLeft || 0));
+        setScrollLeft(containerRef.current?.scrollLeft || 0);
+        console.log("startDragging", pageX, startX, scrollLeft);
+    }
+
+    const onDragging = (e) => {
+        if (!isDragging || !containerRef.current) return;
+        e.preventDefault();
+        const pageX = e.touches ? e.touches[0].pageX : e.pageX;
+        const walk = (pageX - startX) * 1.5;
+        containerRef.current.scrollLeft = scrollLeft - walk;
+    };
+
+    const stopDragging = () => {
+        setIsDragging(false);
+    }
+
     return (
         <Accordion title={title}>
-            <Tabs value={activeTab} className="tabs-skill flex flex-col gap-8 md:gap-12">
-                <TabsHeader className="bg-transparent flex gap-6 md:gap-10 md:pb-0 md:py-0">
+            <Tabs 
+                value={activeTab} 
+                className="tabs-skill flex flex-col gap-8 md:gap-12"
+                
+            >
+                <TabsHeader 
+                    className="bg-transparent flex gap-6 md:gap-10 md:pb-0 md:py-0 overflow-x-auto cursor-grab active:cursor-grabbing"
+                    ref={containerRef}
+                    onMouseDown={startDragging}
+                    onMouseUp={stopDragging}
+                    onMouseLeave={stopDragging}
+                    onTouchStart={startDragging}
+                    onTouchMove={onDragging}
+                    onTouchEnd={stopDragging}
+                >
                     {skills.map(({ ano, titulo }, index) => (
                         <Tab
                             key={index}
