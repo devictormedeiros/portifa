b8914d80721bd4ded47f74be1383b2a08d32d08e
Matheus <matheus@seox.com.br>
2025-06-30 20:03:27 -0300

    Dinamização da seção de Call.

--- src/app/components/Home/Call/Call.js
+++ src/app/components/Home/Call/Call.js
@@ -1,26 +1,25 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const Call = () => {
+const Call = ({ data }) => {
+  console.log(data);
   const canvasRef = useRef(null);
   const images = useRef([]);
   const airpods = useRef({ frame: 0 });
-  const frameCount = 147;
+  const frameCount = data?.frames?.length;
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    const context = canvas.getContext("2d");
     canvas.width = 1158;
     canvas.height = 770;
 
-    const currentFrame = (index) => (
-      `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${(index + 1).toString().padStart(4, '0')}.jpg`
-    );
+    const currentFrame = (index) => data?.frames[index]?.image;
 
     for (let i = 0; i < frameCount; i++) {
       const img = new Image();
@@ -31,8 +30,8 @@ const Call = () => {
 
     gsap.to(airpods.current, {
       frame: frameCount - 1,
-      snap: 'frame',
-      ease: 'none',
+      snap: "frame",
+      ease: "none",
       scrollTrigger: {
         scrub: 0.5,
       },
@@ -48,24 +47,21 @@ const Call = () => {
   }, []);
 
   return (
-    <section className='sec-call bg-[#000]'>
-      <div className='container'>
-        <div className='flex flex-col-reverse md:grid grid-cols-12 gap-y-[2rem] md:gap-x-[2rem]'>
-          <div className='sec-call-text text col-span-12 md:col-span-4'>
-            <div className='h-screen flex items-center'>
-              <h2 className='content-title-h2 text-gray-200 uppercase'>Text</h2>
-            </div>
-            <div className='h-screen flex items-center'>
-              <h2 className='content-title-h2 text-gray-200 uppercase'>Text</h2>
-            </div>
-            <div className='h-screen flex items-center'>
-              <h2 className='content-title-h2 text-gray-200 uppercase'>Text</h2>
-            </div>
-            <div className='h-screen flex items-center'>
-              <h2 className='content-title-h2 text-gray-200 uppercase'>Text</h2>
-            </div>
+    <section className="sec-call bg-[#000]">
+      <div className="container">
+        <div className="flex flex-col-reverse md:grid grid-cols-12 gap-y-[2rem] md:gap-x-[2rem]">
+          <div className="sec-call-text text col-span-12 md:col-span-4">
+            {data?.items?.map((item) => (
+              <>
+                <div className="h-screen flex items-center">
+                  <h2 className="content-title-h2 text-gray-200 uppercase">
+                    {item.text}
+                  </h2>
+                </div>
+              </>
+            ))}
           </div>
-          <div className='sec-call-image image col-span-12 md:col-span-8'>
+          <div className="sec-call-image image col-span-12 md:col-span-8">
             <canvas
               ref={canvasRef}
               className="sticky left-1/2 top-[2.1875rem] max-w-full h-screen max-h-screen object-contain object-right"
