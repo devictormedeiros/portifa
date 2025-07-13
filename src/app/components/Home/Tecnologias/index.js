987e11014f56696f5be9f296ca687aa36ed825d1
Victor Medeiros <victor.desenv97@gmail.com>
2025-07-13 14:29:07 +0200

    QA: correção animacao de progresso [33]

--- src/app/components/Home/Tecnologias/index.js
+++ src/app/components/Home/Tecnologias/index.js
@@ -2,20 +2,27 @@ import Image from "next/image";
 import Accordion from "../../Accordion/Accordion";
 import "./style.scss";
 import { memo } from "react";
+import { useEffect, useState } from "react";
 import ObserverHtml from "../../../hooks/ObserverHtml";
 
 const Tecnologias = ({ data }) => {
+  const [isAnimated, setIsAnimated] = useState(false);
+  const { isVisible, targetRef } = ObserverHtml({ threshold: 0.5 });
+
+  useEffect(() => {
+    if (isVisible && !isAnimated) {
+      setIsAnimated(true);
+    }
+  }, [isVisible, isAnimated]);
+  
   return (
-    <section className="sec-tecnologias g-col-12">
+    <section ref={targetRef} className="sec-tecnologias g-col-12">
       {data?.map((accordion, index) => {
-        // Criar um ObserverHtml separado para cada Accordion
-        const { isVisible, targetRef } = ObserverHtml({ threshold: 0.5 });
 
         return (
           <Accordion key={index} title={accordion.titulo}>
             <ul
-              ref={targetRef} // Agora cada <ul> tem um ref único
-              className={`grid grid-cols-12 gap-y-6 px-6 md:px-0 ${isVisible ? "sec-visible" : ""}`}
+              className={`grid grid-cols-12 gap-y-6 px-6 md:px-0`}
             >
               {accordion.itens?.map((items, idx) => (
                 <li key={idx} className="grid grid-cols-12 col-span-12 md:gap-y-0 gap-y-4 rounded items-center py-0">
@@ -37,7 +44,14 @@ const Tecnologias = ({ data }) => {
                   </div>
                   <div className="md:col-span-9 col-span-12">
                     <div className="progress-bar bg-gray-700 block w-full">
-                      <span className="progress-bar-percent block" style={{ width: `${items.nivel || 0}%` }}></span>
+                    <span
+                        className="progress-bar-percent block"
+                        style={{
+                          width: isAnimated
+                            ? `${items.nivel || 0}%`
+                            : "0%",
+                        }}
+                      ></span>
                     </div>
                   </div>
                 </li>
