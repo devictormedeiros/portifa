8698b06873b5f0ce0c0f1774e2e2b8c22266caa6
Matheus <matheus@seox.com.br>
2025-07-15 02:37:31 -0300

    Adição de classe.

--- src/app/(public)/projeto/[slug]/page.js
+++ src/app/(public)/projeto/[slug]/page.js
@@ -205,7 +205,7 @@ const ProjetoPage = () => {
 
           <section className="w-full mb-[5rem] lg:mb-[7.5rem] w-full max-w-[59.5rem] mx-auto px-6 pt-[3rem]">
             <div
-              className="text-white-70 gutenberg"
+              className="text-white-70 gutenberg overflow-x-visible"
               dangerouslySetInnerHTML={{
                 __html: currentProject.content?.rendered || "",
               }}
